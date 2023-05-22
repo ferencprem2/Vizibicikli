1ffbbe1cb41848bfad3d50b9789ec7854aee3228
ferencprem2 <fercsusz0508@gmail.com>
2023-05-22 14:04:37 +0200

    kesz

--- Kolcsonzes.cs
+++ Kolcsonzes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,42 @@ namespace ConsoleApp1
     internal class Kolcsonzes
     {
         private string nev;
-        private int jazon;
-        private DateOnly eora;
-        private DateOnly eperc;
-        private DateOnly vora;
-        private DateOnly vperc;
+        private char jazon;
+        private int eora;
+        private int eperc;
+        private int vora;
+        private int vperc;
 
-        public string Nev { get => Nev; set => Nev = value; }
-        public int Jazon { get => jazon; set => jazon = value; }
-        public DateOnly Eora { get => eora; set => eora = value; }
-        public DateOnly Eperc { get => eperc; set => eperc = value; }
-        public DateOnly Vora { get => vora; set => vora = value; }
-        public DateOnly Vperc { get => vperc; set => vperc = value; }
+        public int IdoHossz()
+        {
+            return (vora * 60) + vperc - (eora * 60) + eperc;
+        }
+
+        public Kolcsonzes(string csvSor)
+        {
+            var lines = csvSor.Split(';');
+
+            this.nev = lines[0];
+            this.jazon = lines[1][0];
+            this.eora = int.Parse(lines[2]);
+            this.eperc = int.Parse(lines[3]);
+            this.vora = int.Parse(lines[4]);
+            this.vperc = int.Parse(lines[5]);
+
+        }
+        public Kolcsonzes(string nev, char jazon, int eora, int eperc, int vora, int vperc) {
+            this.nev = nev;
+            this.jazon = jazon;
+            this.eora = eora;
+            this.eperc = eperc;
+            this.vora = vora;
+            this.vperc = vperc;
+        }
+        public string Nev { get => nev; set => nev = value; }
+        public char Jazon { get => jazon; set => jazon = value; }
+        public int Eora { get => eora; set => eora = value; }
+        public int Eperc { get => eperc; set => eperc = value; }
+        public int Vora { get => vora; set => vora = value; }
+        public int Vperc { get => vperc; set => vperc = value; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,68 @@
 ﻿using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        List<Kolcsonzes> kolcsonzesek = new();
+        public static List<Kolcsonzes> kolcsonzesek = new();
         static void Main(string[] args)
         {
-            
+            ParseTxtContent();
+
+            //5
+            Console.WriteLine($"5.feladat: {kolcsonzesek.Count()}");
+
+            //6.
+            Console.WriteLine("6.feladat: Kérek egy nevet:");
+            String megadottNev = "Kata";
+            Console.WriteLine(megadottNev);
+            Console.WriteLine($"\t{megadottNev} kölcsönzései:");
+
+            if (kolcsonzesek.Count(x => x.Nev == megadottNev) == 0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző!");
+            }
+            else
+            {
+                kolcsonzesek.Where(x => x.Nev == megadottNev)
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"{x.Eora}:{x.Eperc}-{x.Vora}:{x.Vperc}"));
+            }
+
+
+            //7. feladat
+            Console.Write("7. feladat: Adjon meg egy időpontot óra:perc ablakban: ");
+            string bekertIdo = Console.ReadLine();
+            int bekertOra = Convert.ToInt32(bekertIdo.Split(':')[0]);
+            int bekertPerc = Convert.ToInt32(bekertIdo.Split(':')[1]);
+            int osszido = bekertOra * 60 + bekertPerc;
+
+            kolcsonzesek.Where(x => x.Eora * 60 + x.Eperc < osszido && osszido < x.Vora * 60 + x.Vperc).ToList()
+                .ForEach(x => Console.WriteLine($"{x.Eora}:{x.Eperc}-{x.Vora}:{x.Vperc} : {x.Nev}"));
+
+            //8.
+            int napiBevetel = 2400 * (kolcsonzesek.Sum(ob => ob.IdoHossz()) / 30 + 1);
+            Console.WriteLine($"8. feladat: A napi bevétel: {napiBevetel} Ft");
+
+            //9.
+            StreamWriter sw = new StreamWriter("F.txt");
+            kolcsonzesek.Where(x => x.Jazon == 'F')
+                .ToList()
+                .ForEach(x => sw.WriteLine($"{x.Eora} : {x.Eperc} - {x.Vora}:{x.Vperc} : {x.Nev}"));
+            sw.Close();
+
+            //10
+            kolcsonzesek.GroupBy(x => x.Jazon)
+                .OrderBy(x => x.Key)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}"));
         }
 
 
-        static void ReadFile()
+        static void ParseTxtContent()
         {
-
+            kolcsonzesek = File.ReadAllLines("kolcsonzesek.txt").Skip(1).Select(x => new Kolcsonzes(x)).ToList();
         }
     }
 } 
